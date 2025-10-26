bc713f918b4c76f552dc1cc332b59627da722070
Lucas Hardy <lucashardywork@gmail.com>
2025-10-26 12:17:19 -0300

    Add particle effects

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Animations : MonoBehaviour
 {
     public Animator playerAnim;
+    public GameObject player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
